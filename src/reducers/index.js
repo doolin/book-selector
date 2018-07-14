38db00f747f9820fb7b0e0b2cc4fa321113fd526
dave doolin <david.doolin@gmail.com>
2018-07-14 11:21:25 -0700

    video 59 conditional logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from '../reducers/book_reducer';
+import ActiveBook from './reducer_active_book'
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
